fcb037a5205a6dcedce47b557e4c718cbc266a27
iwts <1065077057@qq.com>
2018-04-21 18:25:43 +0800

    修改为更好的描述

--- LCS.cpp
+++ LCS.cpp
@@ -1,40 +1,46 @@
+/*
+poj-1458 是LCS模板，可以提交测试
+*/
+
 
 #include<iostream>
 #include<string>
+#include<math.h>
+#include<algorithm>
+#define TIME std::ios::sync_with_stdio(false)
 #define MAX 233
 
 using namespace std;
 
-char s1[MAX];
-char s2[MAX];
-int max_len[MAX][MAX];
+string s1,s2;
+int len1,len2;
+int dp[MAX][MAX];
 
 int Max(int a,int b){
-	return a > b ? a : b;
+    return a > b ? a : b;
 }
 
-int main(){
-	while(cin >> s1 >> s2){
-		int len1 = strlen(s1);
-		int len2 = strlen(s2);
-		for(int i = 0;i <= len1;i++){
-			max_len[i][0] = 0;
-		}
-		for(int i = 0;i <= len2;i++){
-			max_len[0][i] = 0;
-		}
-		for(int i = 1;i <= len1;i++){
-			for(int j = 1;j <= len2;j++){
-				if(s1[i-1] == s2[j-1]){
-					max_len[i][j] = max_len[i-1][j-1] + 1;
-				}else{
-					max_len[i][j] = Max(max_len[i-1][j],max_len[i][j-1]);
-				}
-			}
-		}
-		cout << max_len[len1][len2] << endl;
-	}
+int main() {
+    TIME;
+    while(cin >> s1 >> s2){
+        len1 = s1.size();
+        len2 = s2.size();
+        for(int i = 0;i <= len1;i++){
+            dp[i][0] = 0;
+            dp[0][i] = 0;
+        }
+        for(int i = 1;i <= len1;i++){
+            for(int j = 1;j <= len2;j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        cout << dp[len1][len2] << endl;
+    }
 
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
